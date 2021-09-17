6fe017119dfd616bf2ae23ee01e88eb8e1fb6ea8
chengzhengqian-game <chengzhengqian@gmail.com>
2021-09-17 06:10:37 -0400

    update files

---
+++ README.md
@@ -0,0 +1,1 @@
+# CZQUtils.jl
---
+++ src/CZQUtils.jl
@@ -0,0 +1,44 @@
+module CZQUtils
+# old name
+# module JuliaCommon 
+using DelimitedFiles
+import Sockets.Sockets
+export saveData, loadData, linspace, evalInEmacs, sendCommandToEmacaTerm,czqEmacsSocket
+
+"""
+        saveData(data,filename)
+"""
+saveData(data,filename)= open(filename,"w") do io writedlm(io,data) end
+
+"""
+        loadData(filename)
+"""
+loadData(filename)=readdlm(filename)
+
+"""
+generate a linear sequence
+"""
+linspace(start,stop,length)=range(start,stop=stop,length=length)
+
+# czqEmacsSocket=Sockets.connect("localhost",9001)
+
+
+function evalInEmacs(str,s)
+    write(s,str)
+    result_size=parse(Int64,readline(s))
+    result=readline(s)
+    while(length(result)<result_size)
+        result*="\n"*readline(s)
+    end
+    result
+end
+
+# we need to hold it as a dict so we can update it value
+czqEmacsSocket=Dict()
+evalInEmacs(str)=evalInEmacs(str,czqEmacsSocket["default"])
+
+function sendCommandToEmacaTerm(term,command)
+    evalInEmacs("(czq-send-string-term \"$term\"  \"$command\")")
+end
+
+end
